26f57dadecc0115c260e0692d44ab9f0bbd45c2b
geoed2000 <geoed2000@gmail.com>
2018-02-20 12:32:30 +0000

    Added Step 6

---
+++ App1/App1/App1/Step_6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class Step_6 : ContentPage
+    {
+        public Step_6()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
