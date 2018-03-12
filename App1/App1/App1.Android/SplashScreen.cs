e825605801b959d18ff2d248b995840bff751ed5
Charlie N A <lambopuggy@gmail.com>
2018-03-12 14:04:12 +0000

    stuff

--- App1/App1/App1.Android/SplashScreen.cs
+++ App1/App1/App1.Android/SplashScreen.cs
@@ -17,21 +17,26 @@ namespace App1.Droid
     [Activity(Label = "Splash", Theme = "@style/splashScreen", MainLauncher = true, NoHistory = true)]
     public class SplashScreen : Activity
     {
+        int count = 1;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Task startupWork = new Task(() =>
-             {
-                 Task.Delay(3000);
 
-             });
-            startupWork.ContinueWith ( t =>
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() =>
+            {
+                Task.Delay(3000);
+
+            });
+            startupWork.ContinueWith(t =>
             {
                 StartActivity(new Intent(ApplicationContext, typeof(MainActivity)));
-            })
-
-            // Create your application here
-            StartActivity(typeof(MainActivity));
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+            startupWork.Start();
         }
+       
     }
 }
